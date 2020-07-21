126283409f3805d26b7711eb0245bed54a864dd2
AndreSylva <andre.lucyo2@gmail.com>
2020-07-21 17:48:05 -0300

    camada de serviço e registro de componentes-metodos get findAll e findByID

--- src/main/java/com/homeAls/course/repositories/UserRepository.java
+++ src/main/java/com/homeAls/course/repositories/UserRepository.java
@@ -6,6 +6,7 @@ import com.homeAls.course.entities.User;
 
 //cria o repositorie, instancia um objeto com varias operações
 //o spring data ja tem a implementação padrao do jparepositorie
+//nao precisa regstrr como componente, pois ja herda da JpaRepository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/com/homeAls/course/resources/UserResource.java
+++ src/main/java/com/homeAls/course/resources/UserResource.java
@@ -1,26 +1,46 @@
 package com.homeAls.course.resources;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.homeAls.course.entities.User;
+import com.homeAls.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")//passa o caminho do recurso
 public class UserResource {
 
+	//faz a injeção de dependencia para o camada service
+	@Autowired 
+	//a classe de objeto deve ser registrado no mecanismo do framewor
+	//como um componente do spring, la na classe service
+	private UserService service;
+	
 	//Controlador rest que responde no caminho users
 	@GetMapping
-	//Metodo de teste para retornar os usuarios
-	//ResponseEntity - é um tipo do spring par retornar respostas da web
-	public ResponseEntity<User> findAll() {
-		//metodo para ecessar o endpoin do recurso
-		//so para teste criado um user
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		
-		//retorna ok, como o body da resposnta
-		return ResponseEntity.ok().body(u);
+	//ResponseEntity - é um tipo do spring para retornar respostas da web
+	public ResponseEntity<List<User>> findAll() {
+
+		//pega o findAll da camada service
+		List<User> list = service.findAll();
+		//retora a respota de sucessi do http e no corpo da reposta é colocado a lista
+		return ResponseEntity.ok().body(list);
+	}
+	
+	//definir o tipo da requisição do tipo GET, e definir que  requisição tera uma parametro
+	//vai aceitar um ID como parametro
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id)//com a anotação o sring pega o valor e aceita no metodo 
+	{
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
+	
