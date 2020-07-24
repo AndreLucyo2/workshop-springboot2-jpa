4ffe38804d6a0d071d62df80fadfc1128f2c9ae8
Andre Lucyo <andre.lucyo2@gmail.com>
2020-07-24 17:29:05 -0300

    Insercao de user, dequisicao POST ,tratado respota http 201 Createt

--- src/main/java/com/homeAls/course/resources/UserResource.java
+++ src/main/java/com/homeAls/course/resources/UserResource.java
@@ -1,46 +1,56 @@
 package com.homeAls.course.resources;
 
-
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.homeAls.course.entities.User;
 import com.homeAls.course.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")//passa o caminho do recurso
+@RequestMapping(value = "/users") // passa o caminho do recurso
 public class UserResource {
 
-	//faz a injeção de dependencia para o camada service
-	@Autowired 
-	//a classe de objeto deve ser registrado no mecanismo do framewor
-	//como um componente do spring, la na classe service
+	// faz a injeção de dependencia para o camada service
+	@Autowired
+	// a classe de objeto deve ser registrado no mecanismo do framewor
+	// como um componente do spring, la na classe service
 	private UserService service;
-	
-	//Controlador rest que responde no caminho users
+
+	// Controlador rest que responde no caminho users
 	@GetMapping
-	//ResponseEntity - é um tipo do spring para retornar respostas da web
+	// ResponseEntity - é um tipo do spring para retornar respostas da web
 	public ResponseEntity<List<User>> findAll() {
 
-		//pega o findAll da camada service
+		// pega o findAll da camada service
 		List<User> list = service.findAll();
-		//retora a respota de sucessi do http e no corpo da reposta é colocado a lista
+		// retora a respota de sucessi do http e no corpo da reposta é colocado a lista
 		return ResponseEntity.ok().body(list);
 	}
-	
-	//definir o tipo da requisição do tipo GET, e definir que  requisição tera uma parametro
-	//vai aceitar um ID como parametro
+
+	// definir o tipo da requisição do tipo GET, e definir que requisição tera uma
+	// parametro
+	// vai aceitar um ID como parametro
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id)//com a anotação o sring pega o valor e aceita no metodo 
+	public ResponseEntity<User> findById(@PathVariable Long id)// com a anotação o sring pega o valor e aceita no metodo
 	{
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
-	
